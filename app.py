92eaa948ecb2fd7aaf8e441ddc12b948c295affb
aay18-aar12 <133048419+aay18-aar12@users.noreply.github.com>
2024-05-22 18:16:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import math
 app = Flask(__name__)
 
 @app.route('/all')
-def hello_world(page):
+def hello_world():
 
 
     request2 = requests.get('https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Health_WebMercator/MapServer/17/query?where=1%3D1&outFields=*&outSR=4326&f=json')
